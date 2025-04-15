da144f87f1109413b0f84c368dc2263bedee4779
JesusCa1 <s21120013@itsm-tlapa.edu.mx>
2025-04-15 12:05:28 -0600

    Add files via upload

---
+++ RegistroProductos/encabezado.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h2 id="centrado">CONTROL DE PRODUCTOS</h2>
+    <img src="../RegistroProductos/productos/82829150-banner-de-tienda-en-línea-tienda-de-icono-de-construcción-compras-en-línea-de-comestibles-cesta.jpg" width="600" height="100"/>
+    <h4 id="centrado">Registro del nuevo producto</h4>
+</body>
+</html>
